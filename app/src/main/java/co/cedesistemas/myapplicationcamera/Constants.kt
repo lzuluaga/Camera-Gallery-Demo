4c0c7469acc98706e4a6926dd796e078a3d23677
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2021-05-08 10:13:48 -0500

    flujo de camara

--- app/src/main/java/co/cedesistemas/myapplicationcamera/Constants.kt
+++ app/src/main/java/co/cedesistemas/myapplicationcamera/Constants.kt
@@ -2,3 +2,6 @@ package co.cedesistemas.myapplicationcamera
 
 const val REQUEST_CODE_PERMISSION = 2
 const val REQUEST_CODE_GALLERY = 4
+const val REQUEST_CODE_CAMERA = 8
+const val FORMAT_DATE = "yyyyMMdd_HHmmss"
+const val SUFFIX_FILE_NAME = ".jpg"
--- app/src/main/java/co/cedesistemas/myapplicationcamera/MainActivity.kt
+++ app/src/main/java/co/cedesistemas/myapplicationcamera/MainActivity.kt
@@ -2,17 +2,26 @@ package co.cedesistemas.myapplicationcamera
 
 import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.content.FileProvider
 import co.cedesistemas.myapplicationcamera.Permissions.isGrantedPermissions
 import co.cedesistemas.myapplicationcamera.Permissions.verifyPermissions
 import co.cedesistemas.myapplicationcamera.databinding.ActivityMainBinding
 import com.bumptech.glide.Glide
 import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.Date
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private var photoFile: File? =  null
+    private var photoFile: File? = null
     private var attachmentType = AttachmentType.None
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +32,59 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListener() {
-        binding.imgCamera.setOnClickListener {  }
+        binding.imgCamera.setOnClickListener { showCamera() }
         binding.imgGalley.setOnClickListener { showGallery() }
     }
 
+    private fun showCamera() {
+        attachmentType = AttachmentType.Photo
+        if (isGrantedPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            takePhotoIntent()
+        } else {
+            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
+            verifyPermissions(this, permissions)
+        }
+    }
+
+    private fun takePhotoIntent() {
+        photoFile = null
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (intent.resolveActivity(packageManager) != null) {
+            try {
+                photoFile = createImageFile()
+            } catch (exception: Exception) {
+                Toast.makeText(this, "$exception.message", Toast.LENGTH_SHORT).show()
+            }
+            photoFile?.let {
+                val photoUri = FileProvider.getUriForFile(
+                    this,
+                    "co.cedesistemas",
+                    it
+                )
+                val resolveInfoList = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
+                for (resolveInfo in resolveInfoList){
+                    val packageName = resolveInfo.activityInfo.packageName
+                    grantUriPermission(packageName, photoUri,
+                        Intent.FLAG_GRANT_WRITE_URI_PERMISSION or Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri)
+                startActivityForResult(intent, REQUEST_CODE_CAMERA)
+            }
+        }
+    }
+
+    private fun createImageFile(): File? {
+        val imageFileName = "photo_${SimpleDateFormat(FORMAT_DATE).format(Date())}"
+        val storageDirectory: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        storageDirectory?.let {
+            return File.createTempFile(imageFileName, SUFFIX_FILE_NAME, it)
+        } ?: return null
+    }
+
+
     private fun showGallery() {
         attachmentType = AttachmentType.Gallery
-        if (isGrantedPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)){
+        if (isGrantedPermissions(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             showGalleryIntent()
         } else {
             val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -49,17 +104,30 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (requestCode == REQUEST_CODE_PERMISSION && attachmentType == AttachmentType.Gallery){
+        if (requestCode == REQUEST_CODE_PERMISSION && attachmentType == AttachmentType.Gallery) {
             showGalleryIntent()
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_GALLERY){
+        if (requestCode == REQUEST_CODE_GALLERY) {
             getGalleryImage(data)
+        } else if (resultCode == 0 && requestCode == REQUEST_CODE_CAMERA){
+            if (photoFile?.length() == 0L){
+                photoFile?.delete()
+            }
+        }else if (requestCode == REQUEST_CODE_CAMERA){
+            showCameraIntent()
         }
 
+
+    }
+
+    private fun showCameraIntent() {
+        photoFile?.let {
+            showImage(it.path)
+        }
     }
 
     private fun getGalleryImage(data: Intent?) {
